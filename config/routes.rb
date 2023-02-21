3ea77d9c0e6d10bf9f87bec16d2768d1982ebb8a
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-02-20 17:34:01 -0800

    create authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show]
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  delete "/sessions", to: "sessions#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
